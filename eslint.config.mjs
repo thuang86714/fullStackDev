b702c85a0fe1641437b0bbcb76518adb05948883
Hsien Heng Tommy Huang <thuang0714@gmail.com>
2024-04-10 14:39:03 +0800

    fix: remove incorrect import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,28 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+// Importing necessary plugins and configs directly
+import eslintPluginReact from "eslint-plugin-react";
 
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
-
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
-
-export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
-  ...compat.extends("standard-with-typescript"),
-  ...tseslint.configs.recommended,
-  pluginReactConfig,
-];
+export default {
+  files: ["**/*.ts", "**/*.tsx"],
+  parser: "@typescript-eslint/parser",
+  plugins: {
+    "@typescript-eslint": {},
+    "react": {}
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended"
+  ],
+  languageOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
