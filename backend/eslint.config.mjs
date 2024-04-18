bd2db150675901db5fe4746f17b03d9ff9b718a5
Hsien Heng Tommy Huang <thuang0714@gmail.com>
2024-04-18 09:32:04 +0800

    fix

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,29 +1,31 @@
 import globals from "globals";
-import { configs as tseslintConfigs } from "@typescript-eslint/eslint-plugin";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import tseslintPlugin from '@typescript-eslint/eslint-plugin';
 
 import path from "path";
 import { fileURLToPath } from "url";
 
-// mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const { configs: tseslintConfigs } = tseslintPlugin;
+
 export default [
-  {languageOptions: {
-    globals: globals.browser,
-    parser: "@typescript-eslint/parser", // This explicitly sets the parser for TypeScript.
-    ecmaFeatures: {
-      jsx: true  // Support for JSX syntax
-    }
-  }},
-  ...tseslint.configs.recommended,
   {
-    // Integrating React plugin configuration into the flat config
+    languageOptions: {
+      globals: globals.browser,
+      parser: "@typescript-eslint/parser",
+      ecmaFeatures: {
+        jsx: true
+      }
+    }
+  },
+  ...tseslintConfigs.recommended,
+  {
     ...pluginReactConfig,
     settings: {
       react: {
-        version: "detect"  // Automatically detect the React version
+        version: "detect"
       }
     }
   },
