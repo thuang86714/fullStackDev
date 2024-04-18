afc6002670e1da811079322812f484916cc4e371
Hsien Heng Tommy Huang <thuang0714@gmail.com>
2024-04-18 09:37:57 +0800

    fix: simplify eslint config

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,32 +1,18 @@
-import globals from "globals";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import tseslintPlugin from '@typescript-eslint/eslint-plugin';
-
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const { configs: tseslintConfigs } = tseslintPlugin;
-
-export default [
-  {
-    languageOptions: {
-      globals: globals.browser,
-      parser: "@typescript-eslint/parser",
-      ecmaFeatures: {
-        jsx: true
-      }
-    }
+module.exports = {
+  env: {
+    node: true,  // Specifies that the code is running in a Node.js environment
+    es2021: true // Use modern ECMAScript features
   },
-  ...tseslintConfigs.recommended,
-  {
-    ...pluginReactConfig,
-    settings: {
-      react: {
-        version: "detect"
-      }
-    }
+  extends: [
+    'eslint:recommended',  // Start with a set of recommended rules by ESLint
+  ],
+  parserOptions: {
+    ecmaVersion: 12,  // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module'  // Enable ECMAScript modules
   },
-];
+  rules: {
+    // You can customize your rules here. For example:
+    'no-unused-vars': ['warn', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
+    'no-console': 'off',  // Since you're likely using console in the backend development
+  }
+};
